5fd18a68822f6d4078e6a2521ac63973daf345ac
Carl Kashnier <Tetum007@gmail.com>
2016-02-23 11:22:37 -0500

    Revised IP find method request-ip

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ var server = http.createServer(function (req, res) {
 //Parse out language
 var lang = req.headers["accept-language"].substring(0, req.headers["accept-language"].search(","))
 //Parse out IP
-var host = req.socket.remoteAddress;
-//Parse out OS
-var agent = req.headers['x-forwarded-for'] ||
+var host = req.headers['x-forwarded-for'] ||
      req.connection.remoteAddress ||
      req.socket.remoteAddress ||
      req.connection.socket.remoteAddress;
+//Parse out OS
+var agent = req.headers["user-agent"].substring(req.headers["user-agent"].search('\\(') +1, req.headers["user-agent"].search('\\)'))
 //send back JSON object
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.write(JSON.stringify({"ipaddress": host, "language": lang, "Software": agent}));
